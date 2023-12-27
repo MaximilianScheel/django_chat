47300b95eddf18ac39121188b8638449e91518c9
Maximilian Scheel <scheel.development@gmail.com>
2023-12-27 22:05:12 +0100

    Implementing login

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,18 @@
+from django.conf import settings
 from django.db import models
+from django.db.models.fields import DateField
+from datetime import date
 
 # Create your models here.
+
+class Message(models.Model):
+    text = models.CharField(max_length=255)
+    created_at = DateField(default=date.today)
+    chat = models.ForeignKey('Chat', on_delete=models.CASCADE, related_name='chat_messages_set', default=None, blank=True, null=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='author_messages_set')
+    receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='receiver_messages_set')
+    
+    
+class Chat(models.Model):
+    created_at = DateField(default=date.today)
+    
--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
+from .models import Chat, Message
+from django.contrib.auth import authenticate, login
+from django.http.response import HttpResponseRedirect
 
 # Create your views here.
 
-def index(request):
-    return render(request, 'chat/index.html')
+def index(request): # request is an HttpRequest object
+    if request.method == 'POST': # If the form has been submitted as post
+        print(request.POST['textmessage']) # print the textmessage
+        myChat = Chat.objects.get(id=1) # get the chat with id 1
+        Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user) # create a message with the textmessage, the chat with id 1, the author and the receiver
+    chatMessages = Message.objects.filter(chat__id=1) # get the messages with chat id 1
+    return render(request, 'chat/index.html', {'messages': chatMessages}) # render the index.html template with the messages
+
+def login_view(request):
+    if request.method == 'POST':
+        user = authenticate(username=request.POST['username'], password=request.POST['password'])
+        
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect('/chat/')
+        else: 
+            return render(request, 'auth/login.html', {'wrongPassword': True})
+    return render(request, 'auth/login.html')
